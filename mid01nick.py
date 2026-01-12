00540ba270b64c80318f9fc154bb00f7b169f202
Narit1412 <26158@bbw.ac.th>
2026-01-12 15:34:20 +0700

    สร้างโดยใช้ Colab

---
+++ mid01nick.py
@@ -0,0 +1,56 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMKkEHFJyabNhtk77SoYXGw",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Narit1412/01-Nick-coding68/blob/main/mid01nick.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 8,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "aXTMETX3QsAg",
+        "outputId": "17d6f184-6590-41fb-a934-5d06449ab3da"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "200\n",
+            "ออกใบสั่ง\n"
+          ]
+        }
+      ],
+      "source": [
+        "ivelocity = float(input ())\n",
+        "if ivelocity > 120 :\n",
+        "  print('ออกใบสั่ง')"
+      ]
+    }
+  ]
+}
